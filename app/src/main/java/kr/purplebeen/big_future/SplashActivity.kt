7b91a77f9f097b2e054d5829f561276d1437789f
baehyeonbin <baehynebin@hotmail.co.kr>
2018-02-10 20:42:00 +0900

    Facebook SDK Setting

--- app/src/main/java/kr/purplebeen/big_future/SplashActivity.kt
+++ app/src/main/java/kr/purplebeen/big_future/SplashActivity.kt
@@ -2,11 +2,17 @@ package kr.purplebeen.big_future
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //Facebook SDK initialize
+        FacebookSdk.sdkInitialize(getApplicationContext())
+        AppEventsLogger.activateApp(this)
     }
 }
