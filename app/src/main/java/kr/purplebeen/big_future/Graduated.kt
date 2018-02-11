85690506db11324640d1360c0ce4f4d0cde25fc8
gotsejeong <dbdnpxm@naver.com>
2018-02-11 09:51:41 +0900

    master

---
+++ app/src/main/java/kr/purplebeen/big_future/Graduated.kt
@@ -0,0 +1,17 @@
+package kr.purplebeen.big_future
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_graduated.*
+
+class Graduated : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_graduated)
+
+    }
+
+}
